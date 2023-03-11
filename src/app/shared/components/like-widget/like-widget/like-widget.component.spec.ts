e51cb70218d126e42865052188cc5c96eede8849
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-03-11 14:15:54 -0300

    test/likeWidget

--- src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { UniqueIdService } from './../../../services/unique-id/unique-id.service';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
-
+  let fixture: ComponentFixture<LikeWidgetComponent>  = null;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
     fixture = TestBed.createComponent(LikeWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy()
   });
 });
