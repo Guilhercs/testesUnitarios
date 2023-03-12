9fe0c6ebb89e74b027de9a58a837591cd35e6c57
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-03-12 17:55:44 -0300

    fix component

--- src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
@@ -3,29 +3,28 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe('LikeWidgetComponent', () => {
-  let fixture: ComponentFixture<LikeWidgetComponent>  = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LikeWidgetModule],
     }).compileComponents();
     fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
   });
 
   it('Should create component', () => {
-    const instance = fixture.componentInstance;
-    expect(instance).toBeTruthy()
+    expect(component).toBeTruthy();
   });
 
   it('should auto generate ID when id input property is missing', () => {
-    const component = fixture.componentInstance;
     fixture.detectChanges();
     expect(component.id).toBeTruthy();
   });
 
   it('should NOT generate ID when id input property is present', () => {
-    const component = fixture.componentInstance;
-    component.id = 'someId';
     fixture.detectChanges();
+    component.id = 'someId';
     expect(component.id).toBe('someId');
-  })
+  });
 });
